4bbc2169d250c8fd776532ebf0fa40ef306f9f14
Lautner, Josef <josef.lautner+puma@puma.com>
2026-04-22 20:09:37 +0200

    Add SecurityConfig class and update .gitignore for local Maven override

---
+++ src/main/java/com/softwareengineering/petsitter/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.softwareengineering.petsitter.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        http
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .logout(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
+
